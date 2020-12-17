3a92d3b5330e783b6a2525d2373dfdc0ca2461d1
Xuedixuedi <958190137@qq.com>
2020-12-17 20:06:17 +0800

    init back-end

---
+++ datawarehouse/src/main/java/com/example/datawarehouse/DatawarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.example.datawarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatawarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatawarehouseApplication.class, args);
+	}
+
+}
---
+++ datawarehouse/src/test/java/com/example/datawarehouse/DatawarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datawarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatawarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
