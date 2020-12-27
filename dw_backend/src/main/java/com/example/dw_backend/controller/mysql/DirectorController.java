2de6372c50bbedbded46c1f3260751c797e53006
Xuedixuedi <958190137@qq.com>
2020-12-27 23:02:56 +0800

    finish controller test

--- dw_backend/src/main/java/com/example/dw_backend/controller/mysql/DirectorController.java
+++ dw_backend/src/main/java/com/example/dw_backend/controller/mysql/DirectorController.java
@@ -3,10 +3,7 @@ package com.example.dw_backend.controller.mysql;
 import com.example.dw_backend.service.mysql.DirectorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -18,8 +15,9 @@ public class DirectorController {
     @Autowired
     private DirectorService directorService;
 
-    @GetMapping("/director-name/{directorName}")
-    public List<Map<String, String>> getDirectorMovieCount(@PathVariable String directorName) {
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Map<String, String>> getDirectorMovieCount(@RequestParam String directorName) {
         return directorService.parsingDirectorMovie(directorName);
     }
 
