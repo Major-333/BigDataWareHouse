f09e037b8594576494df0ed6ecaa0d657a38f6ea
Major333 <962461330@qq.com>
2020-12-31 16:30:32 +0800

    add apoc for neo4j

---
+++ hive/startup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+hadoop fs -mkdir       /tmp
+hadoop fs -mkdir -p    /user/hive/warehouse
+hadoop fs -chmod g+w   /tmp
+hadoop fs -chmod g+w   /user/hive/warehouse
+
+cd $HIVE_HOME/bin
+./hiveserver2 --hiveconf hive.server2.enable.doAs=false
+
